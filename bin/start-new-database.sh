bdee4d26b8bf557129f232aa39b79ad469dce334
Kyle Pericak <kyle@pericak.com>
2022-01-22 13:50:10 -0500

    comment cleanup in bin/start-new-database.sh

--- bin/start-new-database.sh
+++ bin/start-new-database.sh
@@ -2,11 +2,7 @@
 if [[ "$SQL_PASS" == "" ]]; then echo "ERROR: gotta set SQL_PASS"; exit 1; fi
 if [[ ! -d bin || ! -d kytrade ]]; then echo "ERROR: run this in the base checkout dir"; exit 1; fi
 
-# db data gets saved to database/, it's already in .gitignore
 mkdir -p db-data
-
-
-# NOTE: I tried postgresql but it was just way too slow
 
 docker run -d \
   -p 127.0.0.1:3306:3306 \
@@ -16,24 +12,9 @@ docker run -d \
   -e MYSQL_PASS=$SQL_PASS \
   mysql:latest
 
-# docker exec -u postgres -it trade-sql bash
-
 delay=30
 echo "waiting $delay seconds for db to init"
 sleep $delay
 
 echo "...creating database"
-docker exec -it kytrade-sql \
-  mysql -u root -p$SQL_PASS -e "CREATE DATABASE trade;"
-
-
-# TODO: Remove this - pretty sure sqla will do it now
-#echo "...creating prices_day table"
-#sleep 1
-#docker exec -it trade-sql \
-#  mysql -u root -p$SQL_PASS trade -e "CREATE TABLE prices_day (ticker varchar(8), date DATE, open FLOAT, high FLOAT, low FLOAT, close FLOAT, volume FLOAT);"
-
-#echo "...creating sma table"
-#sleep 1
-#docker exec -it trade-sql \
-#  mysql -u root -p$SQL_PASS trade -e "CREATE TABLE sma (ticker varchar(8), date DATE, style VARCHAR(16), days INT, value FLOAT);"
+docker exec -it kytrade-sql mysql -u root -p$SQL_PASS -e "CREATE DATABASE trade;"

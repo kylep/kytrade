fc8e3c9e9550489767d429c7cec96cb195899f38
Kyle Pericak <kyle@pericak.com>
2022-01-26 09:02:18 -0500

    fixed divide by 0 bug

--- kytrade/calc.py
+++ kytrade/calc.py
@@ -4,5 +4,7 @@
 def compound_anual_growth_rate(begin_value: float, end_value: float, years: int) -> float:
     """Return the CAGR of given values"""
     # https://www.investopedia.com/terms/c/cagr.asp
+    if begin_value == 0:
+        return 0
     return ((end_value / begin_value)**(1 / years) - 1) * 100
 

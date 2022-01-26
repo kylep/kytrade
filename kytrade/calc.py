b57503973bd56601c70c3e182822cb2469bd2154
Kyle Pericak <kyle@pericak.com>
2022-01-26 09:17:27 -0500

    fixed another div by zero bug

--- kytrade/calc.py
+++ kytrade/calc.py
@@ -1,10 +1,10 @@
 """Calculations - Easier to test here than in the portfolio"""
 
 
-def compound_anual_growth_rate(begin_value: float, end_value: float, years: int) -> float:
+def compound_anual_growth_rate(begin_value: float, end_value: float, years: float) -> float:
     """Return the CAGR of given values"""
     # https://www.investopedia.com/terms/c/cagr.asp
-    if begin_value == 0:
+    if begin_value == 0 or years == 0:
         return 0
     return ((end_value / begin_value)**(1 / years) - 1) * 100
 
